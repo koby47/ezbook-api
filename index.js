39862b307fe1e052624b39d1844847253b32da10
koby47 <samuelmensahquaye@gmail.com>
2025-04-21 17:25:44 +0000

    Add cors for integration

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ import errorHandler from "./middlewares/errorhandling.js";
 
 dotenv.config(); // Load environment variables
 
+const allowedOrigins = [
+  "http://localhost:3000", // Dev
+  // Live
+];
 
 const app = express();
-app.use (cors());
+app.use (cors({origin:(origin,callback) =>{
+  if(!origin || allowedOrigins.includes(origin)){
+    callback(null,true);
+  }else{
+    callback(new Error("Not allowed by CORS"));
+
+  }
+},
+credentials:true
+}));
 app.use(express.json());
 
 // Use routers
@@ -33,7 +46,8 @@ const PORT = process.env.PORT || 5000;
 // Async function to connect and start the server
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,
+      useUnifiedTopology: true});
     console.log("Connected to MongoDB");
 
     app.listen(PORT, () => {

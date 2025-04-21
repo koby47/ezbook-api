522087573c2c43a46db25684358ddfb845497acf
koby47 <samuelmensahquaye@gmail.com>
2025-04-21 17:34:51 +0000

    changes to mongodb

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ const PORT = process.env.PORT || 5000;
 // Async function to connect and start the server
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,
-      useUnifiedTopology: true});
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
 
     app.listen(PORT, () => {
